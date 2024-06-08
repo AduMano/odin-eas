15ed982f69b192515c4c83eca1fc981c0c503503
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 05:38:00 +0800

    Optimized the code, somehow it made my performance a bit faster. I learned about delegation and throttle

--- main.js
+++ main.js
@@ -1,62 +1,45 @@
 window.onload = () => {
-    
-    // Generate 16 x 16 grid
     // Init
-    let compGrid = document.querySelector(".grid");
+    const compGrid = document.querySelector(".grid");
     const colors = [
-        "#FF5733",
-        "#33FF57",
-        "#3357FF",
-        "#FF33A1",
-        "#A133FF",
-        "#33FFF1",
-        "#FFC733",
-        "#33FFAC",
-        "#FF5733",
-        "#5733FF",
-        "#FF3357",
-        "#57FF33",
-        "#3357FF",
-        "#FF5733",
-        "#5733FF",
-        "#33FF57",
-        "#FF33A1",
-        "#33FFAC",
-        "#FFC733",
-        "#33FFF1"
+        "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF",
+        "#33FFF1", "#FFC733", "#33FFAC", "#FF5733", "#5733FF",
+        "#FF3357", "#57FF33", "#3357FF", "#FF5733", "#5733FF",
+        "#33FF57", "#FF33A1", "#33FFAC", "#FFC733", "#33FFF1"
     ];
     const setSizeBtn = document.querySelector("button");
 
     // Functions
     const setColor = (e) => {
-        let elem = e.target;
-        let selectedColor = colors[Math.floor(Math.random() * colors.length)];
-        let opacity = parseFloat(elem.getAttribute("data-opacity")) - 0.1;
+        const elem = e.target;
+        if (elem.classList.contains('block')) {
+            const selectedColor = colors[Math.floor(Math.random() * colors.length)];
+            let opacity = parseFloat(elem.getAttribute("data-opacity")) - 0.1;
 
-        elem.style.backgroundColor = selectedColor;
-        elem.style.opacity = "" + opacity;
-        elem.setAttribute("data-opacity", "" + opacity);
-    }
+            elem.setAttribute("data-opacity", `${opacity}`);
+            elem.style.backgroundColor = selectedColor;
+            elem.style.opacity = `${opacity}`;
+        }
+    };
 
     const setGrid = (size) => {
+        const fragment = document.createDocumentFragment();
+
         // Remove All Child
-        while (compGrid.firstChild) {
-            compGrid.removeChild(compGrid.firstChild);
-        }
+        compGrid.innerHTML = '';
 
         // Set Grid
         for (let i = 0; i < (size * size); i++) {
             const block = document.createElement("div");
             block.classList.add("block");
-            // This is to remind the block what opacity 
-            // level it is.
-            block.setAttribute("data-opacity", "1.1"); 
-            block.style.flex = "1 1 calc(100% / " + size + ")";
-            block.addEventListener("mouseover", (e) => {setColor(e)});
+            block.setAttribute("data-opacity", "1");
+            block.style.flex = `1 1 calc(100% / ${size})`;
 
-            compGrid.appendChild(block);
+            fragment.appendChild(block);
         }
-    }
+
+        compGrid.appendChild(fragment);
+    };
 
     // EVENTS
     setSizeBtn.addEventListener("click", () => {
@@ -66,20 +49,20 @@ window.onload = () => {
         // Get Input and Validate
         while (true) {
             getSize = parseInt(prompt("Enter a number to set a grid n x n (1 - 100): "));
-            
-            if (getSize !== NaN && 
-                (getSize >= 1 && getSize <= 100)
-            ) {
+
+            if (!isNaN(getSize) && (getSize >= 1 && getSize <= 100)) {
+                // Set Up Grid
+                setTimeout(() => {
+                    setGrid(getSize);
+                }, 100);
                 break;
-            }
-            else {
+            } else {
                 alert("Please enter between 1 to 100 only");
             }
         }
-
-        // Set Up Grid
-        setGrid(getSize);
     });
 
+    compGrid.addEventListener("mouseover", setColor);
+
     setGrid(16);
-}
+};
