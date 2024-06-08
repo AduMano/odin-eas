55702e29bea24cd855bc93bb44b00c37503a7b99
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 04:43:36 +0800

    Added interactive cells, with color green

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ window.onload = () => {
     let compGrid = document.querySelector(".grid");
 
     // Functions
+    const setColor = (e) => {
+        let elem = e.target;
+        elem.style.backgroundColor = "green";
+    }
+
     const setGrid = (size) => {
         for (let i = 0; i < (size * size); i++) {
             const block = document.createElement("div");
             block.classList.add("block");
             block.style.flex = "1 1 calc(100% / " + size + ")";
+            block.addEventListener("mouseover", (e) => {setColor(e)});
 
             compGrid.appendChild(block);
         }
