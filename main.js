0c02d7e917dcc5f100a2004374726f8d27368c06
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 04:29:41 +0800

    Made a grid generator function

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 window.onload = () => {
-    alert("Test");
+    
+    // Generate 16 x 16 grid
+    // Init
+    let compGrid = document.querySelector(".grid");
+
+    // Functions
+    const setGrid = (size) => {
+        for (let i = 0; i < (size * size); i++) {
+            const block = document.createElement("div");
+            block.classList.add("block");
+            block.style.flex = "1 1 calc(100% / " + size + ")";
+
+            compGrid.appendChild(block);
+        }
+
+    }
+
+    setGrid(16);
 }
